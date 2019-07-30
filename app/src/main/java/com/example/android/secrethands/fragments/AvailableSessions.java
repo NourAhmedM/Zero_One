f6fbe9b14aaf0f982b1f2c60a2a326c351ace39d
Alii-Khaled <id.ali1230@gmail.com>
2019-07-30 14:14:21 +0200

    pushhhh

--- app/src/main/java/com/example/android/secrethands/fragments/AvailableSessions.java
+++ app/src/main/java/com/example/android/secrethands/fragments/AvailableSessions.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.android.secrethands.R;
+import com.example.android.secrethands.adapters.SessionAdapter;
+import com.example.android.secrethands.datastructures.Session;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +29,15 @@ public class AvailableSessions extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_available_sessions, container, false);
+        View view= inflater.inflate(R.layout.fragment_available_sessions, container, false);
+
+        ListView sessionList=(ListView) view.findViewById(R.id.availablesessions);
+        ArrayList<Session> sessionsArray=new ArrayList<>();
+        SessionAdapter adapter=new SessionAdapter(getContext(),0,sessionsArray);
+        sessionList.setAdapter(adapter);
+
+
+        return view;
     }
 
 }
